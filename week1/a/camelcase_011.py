6dc3811b15940f0808e80a0ba1453214238cad51
David Lewinski <79921888+DavidLewinski@users.noreply.github.com>
2021-04-23 22:10:15 +0100

    Delete week1 directory

--- week1/a/camelcase_011.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-import sys
-
-a = sys.stdin.readlines()
-string = "".join(a).rstrip()
-complete = string.title()
-print(complete)
--- week1/a/chop_011.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import sys
-
-for a in sys.stdin:
-    a = a.rstrip()
-    removed = a[1:len(a) - 1]
-    if len(removed) > 0:
-        print(removed)
