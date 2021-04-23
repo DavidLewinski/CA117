cd10d91f71a74a3b0a48be70fbc47f6a45d00f3b
David Lewinski <79921888+DavidLewinski@users.noreply.github.com>
2021-04-23 22:12:49 +0100

    Delete week4 directory

--- week4/b/swap_v2_042.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import sys
-
-def swap_unique_keys_values(d):
-    value = list(d.values())
-    check = {val: 1 for val in value if value.count(val) == 1}
-    swapped = {v: k for (k, v) in d.items() if v in check}
-    return swapped
